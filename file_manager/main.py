735e9bfbd0be0664f9ed4f12505686b0e7a65915
Nick_Bear <bear_76@mail.ru>
2026-07-25 15:30:59 +0300

    Доработана функция move_files и добавлен .gitignore

--- file_manager/main.py
+++ file_manager/main.py
@@ -6,6 +6,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="File manager utility")
     parser.add_argument("--dir", required=True, help="Directory path")
     parser.add_argument("--ext", required=True, help="File extension (e.g. .txt)")
+    parser.add_argument("--target", help="Target directory for the move action")
     parser.add_argument("--yes", action="store_true", help="Skip confirmation prompts")
     parser.add_argument(
         "--action",
@@ -75,18 +76,22 @@ def move_files(
         print(f"Файлов с расширением {extension} не найдено")
         return 0
     # проверка существования директории, в которую будут перемещены файлы
-    if not Path(target_dir).exists():
-        # Если такой директории нет то создать, parent=True - создать родительские папки, exist_ok=True - если папка уже есть ничего не создавать
+    # target_dir - не нужно передавать в экземпляр класса Path, так как он уже является экземпляром этого класса
+    if not target_dir.exists():
+        # Если такой директории нет то создать, parents=True - создать родительские папки, exist_ok=True - если папка уже есть ничего не создавать
         target_dir.mkdir(parents=True, exist_ok=True)
     total_files_moved = 0
-    # Спрашиваем пользователя хочет ли он переместить файлы если нет то выходим и возвращаем ноль перемещенных файлов
-    answer = input("Вы хотите переместить файлы с выбранным расширением (y/n): ")
-    if answer == "n":
-        return total_files_moved
+    # Проверить параметр на согласие перемещения файлов, если не было согласия получено из консоли
+    if not yes:
+        answer = input("Вы хотите переместить файл(ы) с выбранным расширением (y/n): ")
+        # Проверяем только положительный ответ, все остальные будут считаться отрицательными
+        if answer.lower() not in ["yes", "y"]:
+            print("Отменено пользователем")
+            return total_files_moved
     # Перемещаем файлы и увеличиваем счетчик
     for item in directory.iterdir():
         # Проверка действительности файла и его расширения
-        if item.is_file() and item.suffix.lower() == extension:
+        if item.is_file() and item.suffix.lower() == extension.lower():
             # Создание пути для перемещения
             target_path = target_dir / item.name
             # Проверка на существование файла, если существует, по пропускаем
@@ -94,8 +99,15 @@ def move_files(
                 print(f"Файл уже существует: {target_path.name}")
                 continue
             # Переносим файл
-            item.rename(target_path)
-            total_files_moved += 1
+            # обработка ошибок 1 - если не достаточно прав, 2 - ошибка при перемещении
+            try:
+                item.rename(target_path)
+                print(f"Файл {item.name} перемещен -> {target_path}")
+                total_files_moved += 1
+            except PermissionError:
+                print(f"Нет прав на перемещение {item.name}")
+            except Exception:
+                print(f"Возникла ошибка при перемещении {item.name}")
     # Возврат количества перемещенных файлов
     return total_files_moved
 
@@ -125,13 +137,15 @@ def main():
         total = delete_files(directory, extension, args.yes)
         print(f"Удалено файлов: {total}")
     elif args.action == "move":
-        target_dir = args.target
-        if not target_dir:
-            pass
-        total_files_moved = move_files(
-            directory,
-            extension,
-        )
+        # Проверка что в консоли передан параметр --target
+        if not args.target:
+            print("Ошибка: для действия 'move' нужно указать '--target'")
+            return
+        # Создание экземпляра объекта Path
+        target_dir = Path(args.target)
+        # Перемещение файлов и их подсчет
+        total_files_moved = move_files(directory, extension, target_dir, args.yes)
+        print(f"Перемещено - {total_files_moved} файл(ов)")
     else:
         print(f"Действие '{args.action}' пока не реализовано. Скоро будет!")
 
